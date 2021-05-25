338b845bd607d8b21f525ca5af7afe968257c13e
Logan Demmy <frank.demmy@gmail.com>
2021-05-25 10:36:36 -0500

    4 chances to guess

--- Program.cs
+++ Program.cs
@@ -1,30 +1,55 @@
 ﻿using System;
 
+Console.Clear();
 main();
+
 
 void main()
 {
-    
+    int guesses = 0;
+    while (true)
+    {
+
+        
         Console.WriteLine("Hey! Guess my secret number?");
-        Console.Write("What is your guess? ");
-        string guess = Console.ReadLine();
+
+        int guessnum = guess();
         int correctnumber = 42;
 
-        if (int.TryParse(guess, out int guessnum))
+        if (guessnum == correctnumber)
         {
-            if (guessnum == correctnumber)
-            {
-                Console.Write("You guessed Correctly!");
-            }
-            else
-            {
-                Console.WriteLine("You guessed wrong...");
-            }
+            Console.Write("You guessed Correctly!");
+            return;
         }
-    
+
+        else
+        {
+            Console.WriteLine("You guessed wrong...");
+            guesses++;
+        }
+
+        if (guesses == 4)
+        {
+            Console.WriteLine("You lose!!!");
+            return;
+        }
+
+    }
 }
 
-
+int guess()
+{
+    while (true)
+    {
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        ;
+        if (int.TryParse(guess, out int guessnum))
+        {
+            return guessnum;
+        }
+    }
+}
 
 
 
