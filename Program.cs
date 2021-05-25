ff695602cba0c160fca294e06365eef2acfe4c35
Logan Demmy <frank.demmy@gmail.com>
2021-05-25 10:01:51 -0500

    Phase one

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+Console.WriteLine("Hey! Guess my secret number?");
+Console.Write("What is your guess? ");
+
+string guess = Console.ReadLine();
+
+Console.WriteLine($"You just guessed {guess}");
