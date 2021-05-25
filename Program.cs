8d80af9411fecc515b12ddca236072cb3e4cc11c
Logan Demmy <frank.demmy@gmail.com>
2021-05-25 10:57:01 -0500

    guess counter, random number, trys left

--- Program.cs
+++ Program.cs
@@ -7,14 +7,15 @@ main();
 void main()
 {
     int guesses = 0;
+    Console.WriteLine("Hey! Guess my secret number?");
+    Random i = new Random();
+    int correctnumber = i.Next(0,100);
     while (true)
     {
-
-        
+        Console.Clear();
         Console.WriteLine("Hey! Guess my secret number?");
-
-        int guessnum = guess();
-        int correctnumber = 42;
+        int guessnum = guess(guesses);
+        
 
         if (guessnum == correctnumber)
         {
@@ -22,26 +23,36 @@ void main()
             return;
         }
 
-        else
+        else if (guessnum < correctnumber)
         {
-            Console.WriteLine("You guessed wrong...");
             guesses++;
+            Console.WriteLine("Your Guess was too low");
+            Console.WriteLine($"You have {4-guesses} guesses left...");
+             System.Threading.Thread.Sleep(3000);
+        }
+        else 
+        {
+            guesses++;
+            Console.WriteLine("Your Guess was too high");
+            Console.WriteLine($"You have {4-guesses} guesses left...");
+            System.Threading.Thread.Sleep(3000);
         }
 
         if (guesses == 4)
         {
             Console.WriteLine("You lose!!!");
+            Console.WriteLine($"The correct number was {correctnumber}");
             return;
         }
 
     }
 }
 
-int guess()
+int guess(int trynumber)
 {
     while (true)
     {
-        Console.Write("What is your guess? ");
+        Console.Write($"Your guess ({trynumber})? ");
         string guess = Console.ReadLine();
         ;
         if (int.TryParse(guess, out int guessnum))
